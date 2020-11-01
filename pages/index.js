a1cac1742acb196e3507917e5b8a8459012861ad
Jose Cuevas <josecueram@gmail.com>
2020-11-01 17:02:44 -0600

    🚀 Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Consume Xalapa</h1>
+      <h3>Better built this time.</h3>
+      <small>Hopefully...</small>
+    </>
+  );
+};
+
+export default Home;
