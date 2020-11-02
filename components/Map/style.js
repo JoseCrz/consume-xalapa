2ecbf69b586e06944336a0d3e8edac548548f0ef
Jose Cuevas <josecueram@gmail.com>
2020-11-02 17:07:06 -0600

    Add responsive styles to Map component

--- components/Map/style.js
+++ components/Map/style.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
-
+import { phone } from "../../styles/MediaQueries";
 export const Container = styled.div`
   border-radius: 5px;
   overflow: hidden;
   max-width: 80%;
   margin: 0 auto;
+
+  ${phone} {
+    max-width: 100%;
+  }
 `;
