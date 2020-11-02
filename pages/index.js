73b0141fe0101e59885c3b5bea75242886ce8267
Jose Cuevas <josecueram@gmail.com>
2020-11-01 18:11:38 -0600

    Create Basic Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Layout } from "../components/Layout";
 
 const Home = () => {
   return (
-    <>
+    <Layout>
       <h1>Consume Xalapa</h1>
       <h3>Better built this time.</h3>
       <small>Hopefully...</small>
-    </>
+    </Layout>
   );
 };
 
