5d7ba19c89ad1473241c0f448da8031c61779dc8
Jose Cuevas <josecueram@gmail.com>
2020-11-02 17:00:34 -0600

    Add responsive styles to Hero

--- components/Hero/style.js
+++ components/Hero/style.js
@@ -1,19 +1,33 @@
 import styled from "styled-components";
+import { phone } from "../../styles/MediaQueries";
 
 export const Section = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  ${phone} {
+    font-size: 12px;
+  }
 `;
 
 export const H1 = styled.h1`
   font-size: 3em;
   text-align: center;
   margin-bottom: 16px;
+
+  ${phone} {
+    margin: 0;
+  }
 `;
 
 export const P = styled.p`
   font-weight: bold;
   text-align: center;
   max-width: 80%;
+
+  ${phone} {
+    font-size: 1.4em;
+    max-width: 100%;
+  }
 `;
