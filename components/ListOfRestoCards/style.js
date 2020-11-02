5e8aa678a8244bff017dbe1444ff398afb1c9cc4
Jose Cuevas <josecueram@gmail.com>
2020-11-02 17:06:02 -0600

    Add responsive styles to ListOfRestoCards component

--- components/ListOfRestoCards/style.js
+++ components/ListOfRestoCards/style.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
-
+import { phone } from "../../styles/MediaQueries";
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
+
+  ${phone} {
+    grid-template-columns: 1fr;
+    gap: 30px;
+  }
 `;
