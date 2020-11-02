fcdb4d13154ac559051e651eea1e6453ddd931a7
Jose Cuevas <josecueram@gmail.com>
2020-11-02 17:12:38 -0600

    Add responsive styles to Layout component

--- components/Layout/style.js
+++ components/Layout/style.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { phone } from "../../styles/MediaQueries";
 
 export const Main = styled.main`
   width: 100%;
   max-width: 1300px;
   padding: 0 36px;
   margin: 0 auto;
+
+  ${phone} {
+    padding: 0 5px;
+  }
 `;
