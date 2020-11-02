27bced84bafe303658b66bac548bf37a48c9293b
Jose Cuevas <josecueram@gmail.com>
2020-11-01 22:57:47 -0600

    Implement  Basic Data fetching in Home

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Layout } from "../components/Layout";
 import { Hero } from "../components/Hero";
 import { Section } from "../components/Section";
+import { ListOfRestoCards } from "../components/ListOfRestoCards";
 
 const Home = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const { data } = await axios.get("/api/restaurants");
+        setRestaurants(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchRestaurants();
+  }, []);
   return (
     <Layout>
       <Hero />
-      <Section title="Encuentra tu opción ideal"></Section>
+      <Section title="Encuentra tu opción ideal">
+        <ListOfRestoCards restaurants={restaurants} />
+      </Section>
     </Layout>
   );
 };
